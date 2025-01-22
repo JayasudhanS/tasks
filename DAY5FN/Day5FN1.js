dca58c9263f0b58e09de496f0162fda11869dd22
Jayasudhan S <jayasudhan1210@gmail.com>
2025-01-22 09:21:17 +0530

    Delete DAY5FN directory

--- DAY5FN/Day5FN1.js
+++
@@ -1,7 +0,0 @@
-let age=2;
-if (age>=18){
-    console.log("eligible for vote");
-}
-else{
-    console.log("not eligible");
-}
--- DAY5FN/Day5FN6.js
+++
@@ -1,5 +0,0 @@
-//Task 6:loops
-
-for(i=0;i<=10;i++){
-    console.log(i);
-}
